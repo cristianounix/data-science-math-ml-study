de79015b98bfc434dc32754a151b16cf7af8448d
Cristiano S. Oliveira <cristianounix@gmail.com>
2020-01-27 23:59:34 -0300

    Client components (App and Layout)

--- web/client/src/components/Layout/Layout.test.js
+++ web/client/src/components/Layout/Layout.test.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import NotificationTopPage from '../NotificationTopPage/NotificationTopPage';
 import { Layout } from './Layout';
 import classes from './Header.module.scss';
-import MainNotifications from "../../config/mainNotifications";
 
 describe('<Layout />', () => {
 
@@ -24,38 +22,5 @@ describe('<Layout />', () => {
         expect(wrapper.find(Footer)).toHaveLength(1);
     });
 
-    it('should render a notification on development environment', () => {
-        wrapper.setProps({ isProduction: false, environment: 'development' });
-        expect(wrapper.find(NotificationTopPage)).toHaveLength(1);
-        expect(wrapper.exists(`main.${classes.MainSingleNotification}`)).toBe(true);
-    });
-
-    it('should render a notification about DC Password expiry date', () => {
-        wrapper.setProps({
-            currentUser: {
-                mainNotification: {
-                    code: MainNotifications.DC_PASSWORD_EXPIRES,
-                    data: ['2019-01-01']
-                }
-            }
-        });
-        expect(wrapper.find(NotificationTopPage)).toHaveLength(1);
-        expect(wrapper.exists(`main.${classes.MainSingleNotification}`)).toBe(true);
-    });
-
-    it('should render two notifications', () => {
-        wrapper.setProps({
-            isProduction: false,
-            environment: 'development',
-            currentUser: {
-                mainNotification: {
-                    code: MainNotifications.DC_PASSWORD_EXPIRES,
-                    data: ['2019-01-01']
-                }
-            }
-        });
-        expect(wrapper.find(NotificationTopPage)).toHaveLength(2);
-        expect(wrapper.exists(`main.${classes.MainDoubleNotification}`)).toBe(true);
-    });
 })
 
